557543ef763f360becb8909a6c951b86c6e5f4d8
Reed Law <reedlaw@gmail.com>
2011-09-20 11:47:45 -0400

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefTutorial::Application.config.secret_token = '6ecc5d86fe4518a2671d53f633d61cd9e80cea7217bd953568d4186e8eb8d9b06d51ca546af1b04e25358be7fd448befd8c9378f7d18f291b73cd3a4a01ec48d'
