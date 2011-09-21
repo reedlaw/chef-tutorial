65d50ab0301d8b00a65240c8f3d8c52652e4f9bb
Reed Law <reedlaw@gmail.com>
2011-09-21 08:47:47 -0400

    update rails_demo role

--- roles/rails_demo.rb
+++ roles/rails_demo.rb
@@ -1,5 +1,7 @@
 name "rails_demo"
 description "chef tutorial example app"
 run_list(
-  "recipe[application]"         
+         "recipe[application]",
+         "recipe[mongodb::10gen_repo]",
+         "recipe[mongodb]"
 )
