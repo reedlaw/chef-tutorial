9522707ff25ada88177e455f2d8672e2a2f38a00
Reed Law <reedlaw@gmail.com>
2011-09-23 10:51:55 -0400

    update rails_demo cookbook

--- cookbooks/rails_demo/recipes/nginx.rb
+++ cookbooks/rails_demo/recipes/nginx.rb
@@ -1,8 +1,9 @@
-cookbook_file "/etc/nginx/sites-enabled/rails_demo" do
+cookbook_file "#{node["nginx"]["dir"]}/sites-available/rails_demo" do
   source "rails_demo"
   mode 0644
   owner "root"
   group "root"
+  nginx_site "rails_demo"
   notifies :restart, "service[nginx]"
 end
 
